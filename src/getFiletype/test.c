e62edfa637b46589821c803fd5b40b30f776fe67
Runar Buvik <rb@searchdaimon.com>
2007-06-08 11:42:27 +0000

    *** empty log message ***

--- src/getFiletype/test.c
+++ src/getFiletype/test.c
@@ -2,10 +2,11 @@
 #include <stdio.h>
 #include "getfiletype.h"
 
+#include "../common/boithohome.h"
 
 int main()
 {
-    filetypes_info	*fti = getfiletype_init("filetypes.eng.conf");
+    filetypes_info	*fti = getfiletype_init(bfile("config/filetypes.eng.conf"));
 
     printf("Match: %s\n", getfiletype(fti, "pdf"));
 
