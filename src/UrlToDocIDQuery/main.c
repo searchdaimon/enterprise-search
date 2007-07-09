0c41fe783d4f5ff0d8d3a8d0114e6f8fb43b8284
cvsmerge <boitho@searchdaimon.com>
2007-07-09 10:30:38 +0000

    *** empty log message ***

--- src/UrlToDocIDQuery/main.c
+++ src/UrlToDocIDQuery/main.c
@@ -1,19 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 
 #include "../common/crc32.h"
 #include "../common/lot.h"
+#include "../common/bstr.h"
+#include "../getDocIDFromUrl/getDocIDFromUrl.h"
+
+void show (char url[], int n, unsigned int DocID) {
 
 
-int main (int argc, char *argv[]) {
-
-    	
-	unsigned int DocID;
-        
-	if (argc < 2) {
-                printf("Dette programet indekserer en ud file\n\n\tBruke\n\tUrlToDocIDQuery url\n\n");
-                exit(0);
-        }
-
-	if (getDocIDFromUrl(argv[1],&DocID) != 0) {
+	if (n != 0) {
 
 		printf("DocID: %u\n",DocID);
 	}
@@ -21,5 +19,137 @@ int main (int argc, char *argv[]) {
 		printf("ukjent DocID\n");
 	}
 
+}
+
+void showOK (char url[], int n) {
+
+
+	if (n != 0) {
+
+		printf("%-50s [ Found ]\n",url);
+	}
+	else {
+		printf("%-50s [ Not Found ]\n",url);
+
+	}
 
 }
+
+
+int main (int argc, char *argv[]) {
+
+    	
+	char *filename = NULL;
+	char *bdbfiles;
+	unsigned int DocID;
+	int Found, NotFound;
+
+	FILE *FH;
+	int i;
+	char line[512];
+	int n;
+
+	int optShowOK = 0;
+ 	int optFiles = 0;
+
+        extern char *optarg;
+        extern int optind, opterr, optopt;
+        char c;
+	
+        while ((c=getopt(argc,argv,"fk"))!=-1) {
+                switch (c) {
+                        case 'k':
+				optShowOK = 1;
+                                break;
+
+                        case 'f':
+				optFiles = 1;
+                                break;
+
+                        default:
+				printf("wron parameter\n");
+                        	exit(1);
+                }
+        }
+        --optind;
+
+        //printf("argc %i, optind %i\n",argc,optind);
+
+	if (optFiles != 0) {
+
+
+
+		if (argc - optind < 2) {
+               		printf("Dette programet indekserer en ud file\n\n\tBruke\n\tUrlToDocIDQuery -f bdbfiledir file [ file ..n ]\n\n");
+               		exit(0);
+       		}
+
+		bdbfiles = argv[1 + optind];
+
+		printf("bdbfile \"%s\"\n",bdbfiles);
+
+		Found = NotFound = 0;
+		for (i=optind +2;i<argc;i++) {
+
+			
+			filename = argv[i];
+
+			printf("######################################################\n");
+			printf(" Filename \"%s\"\n",filename);
+			printf("######################################################\n");
+
+			if ((FH = fopen(filename,"r")) == NULL) {
+				perror(filename);
+				continue;
+			}
+
+			while (fgets(line,sizeof(line),FH) != NULL) {
+
+				chomp(line);
+		
+				url_normalization(line,sizeof(line));
+
+				//printf("url: \"%s\"\n",line);
+
+				n = getDocIDFromUrl(bdbfiles,line,&DocID);
+
+
+				if (n) {
+					++Found;
+				}
+				else {
+					++NotFound;
+				}
+
+				if (optShowOK) {
+					showOK(line,n);
+
+				}
+				else {
+					show(line,n,DocID);
+				}
+
+			}
+	
+			close(FH);
+
+			printf("Found %i\nNot Found %i\n\n\n",Found,NotFound);
+
+		}
+
+        }
+	else {
+		if (argc != 3) {
+                	printf("Dette programet indekserer en ud file\n\n\tBruke\n\tUrlToDocIDQuery bdbfiledir url\n\n");
+                	exit(0);
+        	}
+
+		n = getDocIDFromUrl(argv[1],argv[2],&DocID);
+
+		show(argv[2],n,DocID);
+
+	}
+	
+}
+
+
