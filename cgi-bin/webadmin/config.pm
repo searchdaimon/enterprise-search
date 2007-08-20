2815d4d3fbe6d3a2361699813bbd985e71da93b0
Dagur Valberg Johannsson <dvj@dagur.com>
2007-08-20 13:58:54 +0000

    boithoad i services listen

--- cgi-bin/webadmin/config.pm
+++ cgi-bin/webadmin/config.pm
@@ -69,7 +69,7 @@ $CONFIG{'valid_auth_methods'} = ['msad', 'ldap', 'shadow'];
 $CONFIG{'rpm_upload_folder'} = "/tmp/rpm";
 
 # Init-services to include in webadmin
-$CONFIG{'init_services'} = ['crawlManager', 'boitho-bbdn', 'searchdbb', 'crawl_watch'];
+$CONFIG{'init_services'} = ['crawlManager', 'boitho-bbdn', 'searchdbb', 'crawl_watch', 'boithoad'];
 
 # Path to init dir with bb services..
 $CONFIG->{'init_dir'} = "/etc/init.d/";
