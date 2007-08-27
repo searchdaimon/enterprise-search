8d47f37735d191ddc129e60cd8dc38b9500a44ae
Eirik Nygaard <eirikald@pvv.ntnu.no>
2007-08-27 19:41:42 +0000

    Add a last argument.

--- src/anchorreadnew/main.c
+++ src/anchorreadnew/main.c
@@ -20,7 +20,7 @@ int main (int argc, char *argv[]) {
 	subname = argv[2];
 
 	//int anchorGetNext (int LotNr,unsigned int *DocID,char *text,unsigned int *radress,unsigned int *rsize)
-	while (anchorGetNextnew(LotNr,&DocID,text,sizeof(text),&radress,&rsize, subname) ) {	
+	while (anchorGetNextnew(LotNr,&DocID,text,sizeof(text),&radress,&rsize, subname, NULL) ) {	
 		printf("DocID %i, text: %s\n",DocID,text);
 	}
 }
