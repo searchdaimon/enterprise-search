639498c433fbc8e0f8e8dbac17ecf17cf85acddd
Dagur Valberg Johannsson <dvj@dagur.com>
2007-08-28 18:17:20 +0000

    lagt til sql_fetch_single

--- Modules/SD/Sql/ConnSimple.pm
+++ Modules/SD/Sql/ConnSimple.pm
@@ -7,60 +7,74 @@ use Carp;
 use DBI;
 use Data::Dumper;
 use Exporter;
-our @EXPORT_OK = qw(sql_setup get_dbh sql_exec sql_fetch_results);
+our @EXPORT_OK = qw(sql_setup get_dbh sql_exec sql_fetch_results sql_fetch_single);
 our @ISA = qw(Exporter);
 
-use constant SQL_CONFIG_FILE   => $ENV{'BOITHOHOME'} . "/config/setup.txt";
+use constant SQL_CONFIG_FILE => $ENV{'BOITHOHOME'} . "/config/setup.txt";
 
 sub sql_setup {
-        my %settings;
+    my %settings;
 
-        open my $setup, SQL_CONFIG_FILE
-		 or croak print "Can't open config file: $!";
-        my @data = <$setup>;
-        close $setup;
+    open my $setup, SQL_CONFIG_FILE
+        or croak print "Can't open config file: $!";
+    my @data = <$setup>;
+    close $setup;
 
-        foreach my $line (@data) {
-                my ($name, $value) = split(/=/, $line);
-                chomp($value) if $value;
-                $settings{$name} = $value if ($name and $value);
-        }
+    foreach my $line (@data) {
+        my ($name, $value) = split(/=/, $line);
+        chomp($value) if $value;
+        $settings{$name} = $value if ($name and $value);
+    }
 
-        $settings{'port'} = 3306 unless($settings{'port'});
+    $settings{'port'} = 3306 unless($settings{'port'});
 
-        return %settings;
+    return %settings;
 }
 
 sub get_dbh {
-	my %setup = @_;
-	my ($db, $host, $port, $user, $pass) = ( $setup{database}, 
-										$setup{server}, $setup{port}, 
-										$setup{user}, $setup{Password} );
+    my %setup = @_;
+    my ($db, $host, $port, $user, $pass) = ( $setup{database}, 
+            $setup{server}, $setup{port}, 
+            $setup{user}, $setup{Password} );
 
     my $dbh = DBI->connect("DBI:mysql:database=$db;host=$host;port=$port", $user, $pass)
-                or croak("$DBI::errstr");
+        or croak("$DBI::errstr");
 
-	return $dbh;
+    return $dbh;
 }
 
 sub sql_exec {
-	my ($dbh, $query, @binds) = @_;
-	my $sth = $dbh->prepare($query)
-		or croak "prepare:", $dbh->errstr;
-	$sth->execute(@binds)
-		or croak "exec: ", $dbh->errstr;
-	return $sth;
+    my ($dbh, $query, @binds) = @_;
+    my $sth = $dbh->prepare($query)
+        or croak "prepare:", $dbh->errstr;
+    $sth->execute(@binds)
+        or croak "exec: ", $dbh->errstr;
+    return $sth;
 }
 
+##
 # Fetch db results for given query and binds
 # to arrayref
 sub sql_fetch_results {
-	my $sth = sql_exec(@_);
-	my @results;
-	while (my $res = $sth->fetchrow_hashref) {
-		push @results, $res;
-	}
-	return @results;
+    my $sth = sql_exec(@_);
+    my @results;
+    while (my $res = $sth->fetchrow_hashref) {
+        push @results, $res;
+    }
+    return @results;
+}
+
+##
+# Fetch only one scalar value.
+#
+# Arguments:
+#   dbh - db connection.
+#   query - SQL Query.
+#   @binds - Bind values.
+sub sql_fetch_single {
+    my $sth = sql_exec(@_);
+    my ($value) = $sth->fetchrow_array;
+    return $value;
 }
 
 1;
