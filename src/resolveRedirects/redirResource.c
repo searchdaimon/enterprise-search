f0af2db6792f02173766ee4c9de818c24baa12cd
Eirik Nygaard <eirikald@pvv.ntnu.no>
2007-09-14 15:05:02 +0000

    A bugfix here and a bugfix there.

--- src/resolveRedirects/redirResource.c
+++ src/resolveRedirects/redirResource.c
@@ -16,6 +16,8 @@ main(int argc, char **argv)
 	char *subname;
 	int LotNr;
 	char filename[1024];
+	char *text;
+	size_t len;
 
 	if (argc < 2)
 		err(1, "Usage: ./readtest lotnr subname");
@@ -29,9 +31,6 @@ main(int argc, char **argv)
 	if ((fp = fopen(filename, "r")) == NULL)
 		err(1, "fopen(%s)", filename);
 
-	char *text;
-	size_t len;
-
 	len = 1048576 * 5; //5MB
 	if ((text = malloc(len)) == NULL) {
 		perror("malloc");
@@ -39,17 +38,20 @@ main(int argc, char **argv)
 	}
 
 	while (fread(&redir, sizeof(redir), 1, fp) == 1) {
+		int len2;
 
 		printf("%u => %u (reason: %hu)\n", redir.DocID, redir.redirectTo, redir.response);
-		readHTMLNET(subname, redir.redirectTo, text, len);
-		addResource(LotNr, subname, redir.DocID, text, strlen(text));
+		len2 = readHTMLNET(subname, redir.redirectTo, text, len);
+		printf("fiii: %d\n", len2);
+		if (text[0] != '\0')
+			addResource(LotNr, subname, redir.DocID, text, len2);
 	//	printf("And got html: ''%s''\n", text);
 	}
-
 	free(text);
 
 	fclose(fp);
 
 	return 0;
 }
+
 
