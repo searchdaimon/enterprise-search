7477c4710b31a91a1a7483347429e2e8f8d337a3
Eirik Nygaard <eirikald@pvv.ntnu.no>
2007-09-24 13:21:08 +0000

    Add a garbage collect summary program.

---
+++ src/gcSummary/gcsummary.c
@@ -0,0 +1,124 @@
+#define __USE_LARGEFILE64 1
+#define _LARGEFILE64_SOURCE
+#define _FILE_OFFSET_BITS 64
+#include <string.h>
+#include <stdio.h>
+#include <err.h>
+#include <unistd.h>
+
+#include "../common/define.h"
+#include "../common/reposetory.h"
+#include "../common/lot.h"
+#include "../common/DocumentIndex.h"
+
+
+int
+main(int argc, char **argv)
+{
+	struct ReposetoryHeaderFormat ReposetoryHeader;
+	int LotNr;
+	char *subname;
+	char htmlbuffer[524288];
+	char imagebuffer[524288];
+	char *acl_allow;
+	char *acl_deny;
+	unsigned long int raddress;
+	char path[1024];
+	char path2[1024];
+	char path3[1024];
+	char sumpath[1024];
+	struct DocumentIndexFormat docindex;
+	unsigned int DocID;
+	FILE *fh, *newfh;
+	
+	if (argc < 3)
+		errx(1, "Usage: ./gcsummary lotnr subname");
+
+	LotNr = atoi(argv[1]);
+	subname = argv[2];
+
+	if ((fh = lotOpenFileNoCasheByLotNr(LotNr,"summary","r",'e',subname)) == NULL) {
+		err(1, "Unable to open summary file");
+	}
+
+	if ((newfh = lotOpenFileNoCasheByLotNr(LotNr,"summary.wip","w",'e',subname)) == NULL) {
+		err(1, "Unable to open summary wip file");
+	}
+
+
+
+	while (DIGetNext(&docindex, LotNr, &DocID, subname)) {
+		char sumbuf[65536];
+		unsigned int len, sDocID;
+		unsigned int ptr;
+
+#if 0
+		printf("Summarygrabing: %d\n", DocID);
+		printf("Seeking to: %x\n", docindex.SummaryPointer);
+#endif
+		len = docindex.SummarySize;
+		if (fseeko64(fh, (off_t)docindex.SummaryPointer, SEEK_SET) == -1) {
+			warn("Unable to seek to summary for %d", DocID);
+			continue;
+		}
+		if (fread(&sDocID, sizeof(sDocID), 1, fh) != 1) {
+			warn("Unable to read docid from summary");
+			continue;
+		}
+		if (sDocID != DocID) {
+			warnx("Did not read the same DocID as was requested");
+			continue;
+		}
+		if (fread(sumbuf, len, 1, fh) != 1) {
+			warn("Unable to read summary");
+			continue;
+		}
+
+		/* Write new summary */
+		docindex.SummaryPointer = ftell(newfh);
+		fwrite(&DocID, sizeof(DocID), 1, newfh);
+		fwrite(sumbuf, docindex.SummarySize, 1, newfh);
+		//DIWrite(&docindex, DocID, subname, NULL);
+	}
+#if 0
+	while (rGetNext(LotNr,&ReposetoryHeader,htmlbuffer,sizeof(htmlbuffer),imagebuffer,&raddress,0,0,subname,&acl_allow,&acl_deny)) {
+
+
+		if (!DIRead(&docindex, ReposetoryHeader.DocID, subname)) {
+			fprintf(stderr, "Unable to locate a DI for %d\n", ReposetoryHeader.DocID);
+			continue;
+		}
+
+		//printf("%p\n", docindex.RepositoryPointer);
+		if (raddress == docindex.RepositoryPointer) {
+			unsigned long int offset;
+			offset = rApendPost(&ReposetoryHeader, htmlbuffer, imagebuffer, subname, acl_allow, acl_deny, "repo.wip");
+			docindex.RepositoryPointer = offset;
+			DIWrite(&docindex, ReposetoryHeader.DocID, subname, "DocumentIndex.wip");
+			printf("Writing DocID: %d\n", ReposetoryHeader.DocID);
+		} else {
+			printf("Garbage collecting %d at %lx\n", ReposetoryHeader.DocID, raddress);
+		}
+	}
+#endif
+
+	/* And we have a race... */
+#if 0
+	GetFilPathForLot(path, LotNr, subname);
+	strcpy(path2, path);
+	strcpy(path3, path);
+	strcat(path, "repo.wip");
+	strcat(path2, "reposetory");
+	rename(path, path2);
+	strcpy(path, path3);
+	strcat(path, "DocumentIndex.wip");
+	strcat(path3, "DocumentIndex");
+	rename(path, path3);
+#endif
+
+	fclose(fh);
+	fclose(newfh);
+
+	return 0;
+}
+
