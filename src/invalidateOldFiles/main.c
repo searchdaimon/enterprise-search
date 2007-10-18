5f915541ec55a354836021bbb02f27ee00125a11
Eirik Nygaard <eirikald@pvv.ntnu.no>
2007-10-18 10:12:00 +0000

    Add dry-run argument. Set the correct values when invalidating the docindex.

--- src/invalidateOldFiles/main.c
+++ src/invalidateOldFiles/main.c
@@ -20,25 +20,35 @@ main(int argc, char **argv)
 	char *subname;
 	struct DocumentIndexFormat docindex;
 	time_t timeout;
+	int dry;
 
         //tester for at vi har fåt hvilken lot vi skal bruke
         if (argc < 3)
-                errx(1, "Usage: ./%s lotnr subname\n\n", argv[0]);
+                errx(1, "Usage: ./%s lotnr subname [dry]", argv[0]);
+
+	if (argc > 3)
+		dry = 1;
+	else
+		dry = 0;
 
 	LotNr = atoi(argv[1]);
 	subname = argv[2];
 
 	timeout = time(NULL) - TIMEOUT;
+	if (dry)
+		printf("Doing a dry run, won't write updated document indexes.\n");
+
 	while (DIGetNext(&docindex, LotNr, &DocId, subname)) {
-		/* Skip if we don't have a resource set */
-		if (docindex.ResourceSize == 0)
+		if (docindex.htmlSize == 0)
 			continue;
 		/* Invalidate? */
 		if (docindex.lastSeen < timeout) {
 			printf("Invalidating %u...\n", DocId);
-			docindex.ResourcePointer = 0;
-			docindex.ResourceSize = 0;
-			DIWrite(&docindex, DocId, subname, NULL);
+			docindex.RepositoryPointer = 0;
+			docindex.htmlSize = 0;
+			if (!dry) {
+				DIWrite(&docindex, DocId, subname, NULL);
+			}
 		}
 	}
 
