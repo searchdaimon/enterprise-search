2c44bca2d9d6b6fb1d9be3ebacb3fdbe4c072414
Dagur Valberg Johannsson <dvj@dagur.com>
2007-12-05 21:09:13 +0000

    Begrense chr i navn

--- cgi-bin/webadmin/Common/Collection.pm
+++ cgi-bin/webadmin/Common/Collection.pm
@@ -5,47 +5,43 @@ use Data::Dumper;
 use Sql::Shares;
 use Carp;
 
-# This class provides a set of common features regarding collections.
+my @VALID_COLL_CHRS 
+    = ('a'..'z', 'A'..'Z', 0..9, '-', q{ }, '_');
+
+my $sqlShares;
 
 sub new {
-	my $class = shift;
-	my $self = {};
-	bless $self, $class;
-	$self->_init(@_);
-	return $self;
+    my $class = shift;
+    my $self = {};
+    bless $self, $class;
+    $self->_init(@_);
+    return $self;
 }
 
 sub _init {
-	my $self = shift;
-	my $dbh = shift;
-	$self->{'dbh'} = $dbh;
-	$self->{'sqlShares'} = Sql::Shares->new($dbh);
+    my ($self, $dbh) = @_;
+    $sqlShares = Sql::Shares->new($dbh);
 }
 
-# Checks for error in input from a form. Returns two values. 
-# One true or false valid value, and one more describing string.
-# If the check collection_name is used, $sqlShares needs to be defined.
-# Example usage: my ($valid, $msg) = Collection::validate_share($dbh, $share, @checks);
+##
+# Check if a collection being submitted is valid.
+# Returns:
+#   valid - true/false
+#   err   - reason for error
 sub validate {
-	my $self = shift;
-	my $share = shift;
-	my $sql = $self->{'sqlShares'};
-	my @checks = (@_);
-		
-	# Add default values if checks aren't provided.
-	unless (@checks) {
-		@checks = ('host', 'collection_name', 'connector');
-	}
-	
-	# note to self: add uses connector and collection_name in first form.
-	
-	while (my $check = shift(@checks)) {
-		
-		if ($check eq 'share') {
-			next if ($share->{'host'});
-			next if ($share->{'resource'});
-			return (0, 'error_missing_share');
-		}
+    my ($self, $share, @checks) = @_;
+
+    unless (@checks) { # default checks
+        @checks = ('host', 'collection_name', 'connector');
+    }
+
+    while (my $check = shift(@checks)) {
+
+        if ($check eq 'share') {
+            next if ($share->{'host'});
+            next if ($share->{'resource'});
+            return (0, 'error_missing_share');
+        }
 # 		if ($check eq 'host') {
 # 			return (0, 'error_missing_host') 
 # 				unless ($share->{'host'});
@@ -55,29 +51,33 @@ sub validate {
 # 			return (0, 'error_missing_resource')
 # 				unless ($share->{'resource'});
 # 		} 
-		
-		elsif ($check eq 'connector') {
-			return (0, 'error_missing_connector')
-				unless ($share->{'connector'});
-		} 
-		
-		elsif ($check eq 'collection_name') {
-			my $name = $share->{'collection_name'};
-			my $id = $share->{'id'};
-			
-			if ($sql->get_id_by_collection($name)) {
-				# Is it I that hold the collection?
-				unless ($id and $sql->get_collection_name($id) eq $name) {
-					return (0, 'error_collection_exists');
-				}
-			}
-		}
-		
-		else {
-			carp "Unknown check $check requested.";
-		}
-	}
-	return (1, 'valid');
+
+        elsif ($check eq 'connector') {
+            return (0, 'error_missing_connector')
+                unless ($share->{'connector'});
+        } 
+
+        elsif ($check eq 'collection_name') {
+            my $name = $share->{'collection_name'};
+            my $id = $share->{'id'};
+
+            foreach my $c (split '', $name) {
+                return (0, 'error_inv_name')
+                    unless grep { "$c" eq "$_" } @VALID_COLL_CHRS;
+            }
+
+            if ($sqlShares->get_id_by_collection($name)) {
+                unless ($id and $sqlShares->get_collection_name($id) eq $name) {
+                    return (0, 'error_collection_exists');
+                }
+            }
+        }
+
+        else {
+            carp "Unknown check $check requested.";
+        }
+    }
+    return (1, 'valid');
 }
 
 1;
