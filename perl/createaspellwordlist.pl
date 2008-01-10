25e16b22c5f0290a93b77d35835f00bfa9485097
Eirik Nygaard <eirikald@pvv.ntnu.no>
2008-01-10 15:17:59 +0000

    Add spelling scripts.

---
+++ perl/createaspellwordlist.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/env perl -w
+
+use warnings;
+use strict;
+
+while (<>) {
+	my @a = split;
+	print $a[0]."\n";
+}
---
+++ perl/filterDictionaryForSpelling.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/env perl -w
+
+use strict;
+use warnings;
+
+while (<>) {
+	my @all = split;
+	print $all[0]."\n";
+}
+
