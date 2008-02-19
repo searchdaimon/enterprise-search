26dab2b9eec835d5c0f07743d3b2b52c453a32a3
cvsmerge <boitho@searchdaimon.com>
2008-02-19 09:40:34 +0000

    *** empty log message ***

---
+++ blackbox/filepermissions_fix.sh
@@ -0,0 +1,8 @@
+chown boitho:boitho /home/boitho/boithoTools/
+chown boitho:boitho /home/boitho/boithoTools/bin
+chown boitho:boitho /home/boitho/boithoTools/var
+chown boitho:boitho /home/boitho/boithoTools/var/*
+chown boitho:boitho /home/boitho/boithoTools/logs
+chown boitho:boitho /home/boitho/boithoTools/logs/*
+
+chown -R boitho:boitho /boithoData/lot/
