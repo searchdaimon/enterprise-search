ec6ecbc858aa39e8dae3dad76befee5d9dad8e7d
Eirik Nygaard <eirikald@pvv.ntnu.no>
2008-03-27 15:24:02 +0000

    Only push iso-8859-1 data to aspell.

--- perl/createaspellwordlist.pl
+++ perl/createaspellwordlist.pl
@@ -3,14 +3,23 @@
 use warnings;
 use strict;
 
+use Text::Iconv;
+my $converter = Text::Iconv->new("utf-8", "iso-8859-1");
+#my $converter = Text::Iconv->new("utf-8", "utf-8");
+
 while (<>) {
 	my @a = split;
+	my $w = $converter->convert($a[0]);
+	#my $w = $a[0];
+	if (not defined $w) { # Unable to convert
+		next;
+	}
 	# Magnus: Kun ord som begynner på a-z, og som ikke inneholder siffer eller '.
 	# Fjerner også ord på to bokstaver, da aspell foreslår mange rare rettskrivinger med de:
-#	if (not substr($a[0],0,1)=~ /[^a-z]/ and not $a[0]=~ /[\'0-9]/ and length($a[0])>2)
+	if (not $w =~ /^[^a-z]/ and not $w =~ /[\'0-9]/ and length($w) > 2)
 	# Magnus: Vi fjerner utf8-tegn midlertidig, ser ut til å skape en del problemer:
-	if (not $a[0]=~ /[^a-z]/ and length($a[0])>2)
+#	if (not $a[0] =~ /[^a-z]/ and length($a[0])>2)
 	    {
-		print $a[0]."\n";
+		print $w."\n";
 	    }
 }
