ee78221c384d40612d0fcf04dc4acf738eb2f547
Eirik Nygaard <eirikald@pvv.ntnu.no>
2008-03-31 10:38:47 +0000

    Set datadir for aspell Copy and remove, not move the generated dictionary.

--- script/makebbwordlist.sh
+++ script/makebbwordlist.sh
@@ -4,5 +4,6 @@
 BBH="$BOITHOHOME"
 LIBASPELL="$BBH/data/dict"
 
-perl $BBH/perl/createaspellwordlist.pl $BBH/var/dictionarywords | aspell --encoding=iso-8859-1 --lang=bb create master /tmp/bb.rws
-mv /tmp/bb.rws $LIBASPELL
+perl $BBH/perl/createaspellwordlist.pl $BBH/var/dictionarywords | aspell --data-dir=$BBH/data/dict --encoding=iso-8859-1 --lang=bb create master /tmp/bb.rws
+cp /tmp/bb.rws $LIBASPELL
+rm /tmp/bb.rws
