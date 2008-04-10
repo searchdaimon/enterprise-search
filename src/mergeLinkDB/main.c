bd55689ebfdee7c2600e0c54ffcc75b1266b5a72
cvsmerge <boitho@searchdaimon.com>
2008-04-10 11:35:22 +0000

    *** empty log message ***

--- src/mergeLinkDB/main.c
+++ src/mergeLinkDB/main.c
@@ -7,78 +7,105 @@
 #include <string.h>
 #include <unistd.h>
 
-#include "main.h"
+#define BLOCK_A    1
+#define BLOCK_B    2
+#define BLOCK_BOTH 3
 
-int main (int argc, char *argv[]) {
-	
-	FILE * linkfile_a; 
-	FILE * linkfile_b; 
-        FILE *outfh;
-
-        char *outfile = strdup("/tmp/sdweb-XXXXXX");
-        int outfd;
-
-	if (argc < 3) {
-   		fprintf(stderr, "Programmet tar inn to linkdb filer og slaar de sammen");
-		fprintf(stderr, "til ny (sortert) fil\n\nUsage: ./mergeLinkDB output linkdb\n");
-		exit(EXIT_FAILURE);
-    	}
-	
-	//skriver ut lit status slik at vi kan se progresjon hvis vi kjører dette fra et skript
-	printf("merging %s < %s\n",argv[1],argv[2]);
-
-	linkfile_a = open_db_file(argv[1]);
-	linkfile_b = open_db_file(argv[2]);
+#include "../common/define.h"
 
 
+static inline int int_compare(unsigned int a, unsigned int b) {
+	if (a < b)
+		return -1;
+	if (a > b)
+		return 1;
 
-	//lager en temperert navn
-        if ((outfd = mkstemp(outfile)) == -1) {
-                perror("mkstemp");
-		exit(EXIT_FAILURE);
-        }
-
-
-	//åpner filen med det temerere navnet
-        if ((outfh = fdopen(outfd,"w")) == NULL) {
-                perror("mkstemp");
-		exit(EXIT_FAILURE);
-        }
-
-	#ifdef debug
-        printf("tmp outfile: \"%s\"\n",outfile);
-	#endif
-
-	/*
-	char * outfile = argv[3];
-	if ((output = fopen(outfile, "wb")) == NULL) {
-		fprintf(stderr, "Unable to write to file %s", outfile);
-		perror(outfile);
-		exit(EXIT_FAILURE);
-	}
-	*/
-
-	run_merge(linkfile_a, linkfile_b, outfh);
-
-	//må lokke før vi kopierer. Hvis ikke kan det være data i bufferen som ikke blir med.
-	fclose(outfh);
-	close(outfd);
-
-	//sletter den gamle filen
-	if (unlink(argv[1]) != 0) {
-		perror(argv[1]);
-	}
-
-	//flytter den nye filen slik at den blir output filen
-	if (rename(outfile,argv[1]) == -1) {
-		perror("rename");
-		exit(EXIT_FAILURE);
-	}
-
-	free(outfile);
-
-	return 1;
+	return 0;
 }
+
+/**
+ * Reads next block(s) from db file(s)
+ */
+static inline void read_next_blocks(struct linkdb_block * block_a, struct linkdb_block * block_b, 
+				FILE * file_a, FILE * file_b, int block_out) {		
+	switch(block_out) {
+		case BLOCK_A:
+			fread(block_a, sizeof(*block_a), 1, file_a);
+			break;
+		case BLOCK_B:
+			fread(block_b, sizeof(*block_b), 1, file_b);
+			break;
+		case BLOCK_BOTH:
+			fread(block_a, sizeof(*block_a), 1, file_a);
+			fread(block_b, sizeof(*block_b), 1, file_b);
+	}
+}
+
+/**
+ * Determines what block(s) should be written based on DocID_from.
+ */
+static inline int find_by_from(struct linkdb_block * block_a, struct linkdb_block * block_b) {
+	int order = int_compare(block_a->DocID_from, block_b->DocID_from);
+	int block_out;
+	switch(order) {
+		case -1:
+			block_out = BLOCK_A;
+			break;
+		case 0:
+			block_out = BLOCK_BOTH;
+			break;
+		case 1:
+			block_out = BLOCK_B;
+			break;
+	}
+
+	return block_out;
+}
+
+/**
+ * Determines what block(s) should be written
+ * to output.
+ */
+static inline int find_next_block(struct linkdb_block * block_a, struct linkdb_block * block_b) {
+	int block_out;
+	int order = int_compare(block_a->DocID_to, block_b->DocID_to);
+	switch (order) {
+		case -1:
+			block_out = BLOCK_A;
+			break;
+		case 0:
+			block_out = find_by_from(block_a, block_b);
+			break;
+		case 1:
+		block_out = BLOCK_B;
+			break;
+	}
+	return block_out;
+}
+
+static inline void write_out(FILE * output, struct linkdb_block * db_block) {
+	fwrite(db_block, sizeof(struct linkdb_block), 1, output);
+}
+
+/**
+ * Dump all remaining content of the file that is not at eof.
+ */
+static inline void dump_remaining(FILE * file_a, FILE * file_b, FILE * out) {
+	FILE * in;
+	if (feof(file_a) && feof(file_b))
+		return;
+
+	in = (feof(file_a)) ? file_b : file_a;
+
+	struct linkdb_block block;
+	while (!feof(in)) {
+		fread(&block, sizeof(block), 1, in);
+		write_out(out, &block);
+	}
+}
+
+
+
 
 /**
  * Main merge loop.
@@ -127,95 +154,8 @@ void run_merge(FILE * file_a, FILE * file_b, FILE * output) {
 	fclose(file_b);
 }
 
-/**
- * Determines what block(s) should be written
- * to output.
- */
-int find_next_block(struct linkdb_block * block_a, struct linkdb_block * block_b) {
-	int block_out;
-	int order = int_compare(block_a->DocID_to, block_b->DocID_to);
-	switch (order) {
-		case -1:
-			block_out = BLOCK_A;
-			break;
-		case 0:
-			block_out = find_by_from(block_a, block_b);
-			break;
-		case 1:
-		block_out = BLOCK_B;
-			break;
-	}
-	return block_out;
-}
 
-/**
- * Reads next block(s) from db file(s)
- */
-void read_next_blocks(struct linkdb_block * block_a, struct linkdb_block * block_b, 
-				FILE * file_a, FILE * file_b, int block_out) {		
-	switch(block_out) {
-		case BLOCK_A:
-			fread(block_a, sizeof(*block_a), 1, file_a);
-			break;
-		case BLOCK_B:
-			fread(block_b, sizeof(*block_b), 1, file_b);
-			break;
-		case BLOCK_BOTH:
-			fread(block_a, sizeof(*block_a), 1, file_a);
-			fread(block_b, sizeof(*block_b), 1, file_b);
-	}
-}
 
-/**
- * Determines what block(s) should be written based on DocID_from.
- */
-int find_by_from(struct linkdb_block * block_a, struct linkdb_block * block_b) {
-	int order = int_compare(block_a->DocID_from, block_b->DocID_from);
-	int block_out;
-	switch(order) {
-		case -1:
-			block_out = BLOCK_A;
-			break;
-		case 0:
-			block_out = BLOCK_BOTH;
-			break;
-		case 1:
-			block_out = BLOCK_B;
-			break;
-	}
-
-	return block_out;
-}
-
-/**
- * Dump all remaining content of the file that is not at eof.
- */
-void dump_remaining(FILE * file_a, FILE * file_b, FILE * out) {
-	FILE * in;
-	if (feof(file_a) && feof(file_b))
-		return;
-
-	in = (feof(file_a)) ? file_b : file_a;
-
-	struct linkdb_block block;
-	while (!feof(in)) {
-		fread(&block, sizeof(block), 1, in);
-		write_out(out, &block);
-	}
-}
-
-int int_compare(unsigned int a, unsigned int b) {
-	if (a < b)
-		return -1;
-	if (a > b)
-		return 1;
-
-	return 0;
-}
-
-void write_out(FILE * output, struct linkdb_block * db_block) {
-	fwrite(db_block, sizeof(struct linkdb_block), 1, output);
-}
 
 /**
  * Helper function to open a db file.
@@ -223,12 +163,99 @@ void write_out(FILE * output, struct linkdb_block * db_block) {
 FILE * open_db_file(const char * filename) {
     FILE * fh;
     if ((fh = fopen(filename, "rb")) == NULL) {
-		fprintf(stderr, "Unable to open file. ");
-		perror(filename);
-		exit(EXIT_FAILURE);
+		//fprintf(stderr, "Unable to open file. ");
+		//perror(filename);
+		//exit(EXIT_FAILURE);
     }
 
     return fh;
 }
 
+
+int main (int argc, char *argv[]) {
+	
+	FILE * linkfile_a; 
+	FILE * linkfile_b; 
+        FILE *outfh;
+
+        char *outfile = strdup("/tmp/sdweb-XXXXXX");
+        int outfd;
+	char sysbuf[512];
+
+	if (argc < 3) {
+   		fprintf(stderr, "Programmet tar inn to linkdb filer og slaar de sammen");
+		fprintf(stderr, "til ny (sortert) fil\n\nUsage: ./mergeLinkDB output linkdb\n");
+		exit(EXIT_FAILURE);
+    	}
+	
+	//skriver ut lit status slik at vi kan se progresjon hvis vi kjører dette fra et skript
+	printf("merging %s < %s\n",argv[1],argv[2]);
+
+	if ((linkfile_a = open_db_file(argv[1])) == NULL) {
+		printf("dosent have a link db. Creating one from \"%s\"\n",argv[2]);
+		sprintf(sysbuf,"cp %s %s",argv[2],argv[1]);
+		printf("runing: \"%s\"\n",sysbuf);
+		system(sysbuf);
+		exit(1);
+	}
+
+	if ((linkfile_b = open_db_file(argv[2])) == NULL) {
+		perror(argv[2]);
+		exit(1);
+	}
+
+
+
+	//lager en temperert navn
+        if ((outfd = mkstemp(outfile)) == -1) {
+                perror("mkstemp");
+		exit(EXIT_FAILURE);
+        }
+
+
+	//åpner filen med det temerere navnet
+        if ((outfh = fdopen(outfd,"w")) == NULL) {
+                perror("mkstemp");
+		exit(EXIT_FAILURE);
+        }
+
+	#ifdef debug
+        printf("tmp outfile: \"%s\"\n",outfile);
+	#endif
+
+	/*
+	char * outfile = argv[3];
+	if ((output = fopen(outfile, "wb")) == NULL) {
+		fprintf(stderr, "Unable to write to file %s", outfile);
+		perror(outfile);
+		exit(EXIT_FAILURE);
+	}
+	*/
+
+	run_merge(linkfile_a, linkfile_b, outfh);
+
+	//må lokke før vi kopierer. Hvis ikke kan det være data i bufferen som ikke blir med.
+	fclose(outfh);
+	close(outfd);
+
+	//sletter den gamle filen
+	if (unlink(argv[1]) != 0) {
+		perror(argv[1]);
+	}
+
+	//flytter den nye filen slik at den blir output filen
+	sprintf(sysbuf,"mv %s %s",outfile,argv[1]);
+	printf("runing: \"%s\"\n",sysbuf);
+	system(sysbuf);
+	/*
+	if (rename(outfile,argv[1]) == -1) {
+		perror("rename");
+		exit(EXIT_FAILURE);
+	}
+	*/
+
+	free(outfile);
+
+	return 1;
+}
 
