b4309f95c1bb1e3fe4b373d0367e67897b484139
cvsmerge <boitho@searchdaimon.com>
2008-04-18 13:43:35 +0000

    *** empty log message ***

--- src/readDocumentIndexWithRank/main.c
+++ src/readDocumentIndexWithRank/main.c
@@ -14,25 +14,36 @@ int main (int argc, char *argv[]) {
 	unsigned int DocID;
 	int rank;
 
-        if (argc < 3) {
-                printf("Dette programet leser en DocumentIndex. Gi det et lot nr. \n\n\tUsage: ./readDocumentIndex lotNr subname minrank\n");
+        if (argc < 4) {
+                printf("Dette programet leser en DocumentIndex. Gi det et lot nr. \n\n\tUsage: ./readDocumentIndex lotNr subname minrank maxrank\n");
                exit(0);
         }
 
 	LotNr = atoi(argv[1]);
 	char *subname = argv[2];
 	int minrank = atoi(argv[3]);
+	int maxrank = atoi(argv[4]);
 	DocID = 0;
 
         popopenMemArray_oneLot(subname, LotNr);
 
 	while (DIGetNext (&DocumentIndexPost,LotNr,&DocID,subname)) {
 
+		if (DocumentIndexPost.response != 200) {
+			continue;
+		}
+		//printf("response: %i\n",(int)DocumentIndexPost.response);
+
 		rank = popRankForDocIDMemArray(DocID);
 
-		if (rank >= minrank) {
+		if ((rank >= minrank) && (rank <= maxrank)) {
 			//printf("DocID: %u, url: %s, rank %i\n",DocID,DocumentIndexPost.Url,rank);
+			//printf("rank %i\n",rank);
 			printf("%s\n",DocumentIndexPost.Url);
+		}
+		else {
+			//printf("not in range: rank %i\n",rank);
+
 		}
 	}
 
