e0cacb18f235c2a513b9b90d09639252532c4208
cvsmerge <boitho@searchdaimon.com>
2008-04-18 13:55:03 +0000

    Har flyttet selve gc koden til src/common/gc.c.

--- src/gcRepo/gcrepo.c
+++ src/gcRepo/gcrepo.c
@@ -1,29 +1,17 @@
-
+#include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
 #include <err.h>
 
-#include "../common/define.h"
-#include "../common/reposetory.h"
-#include "../common/lot.h"
-#include "../common/DocumentIndex.h"
-
-
+#include "../common/gcrepo.h"
 
 int
 main(int argc, char **argv)
 {
-	struct ReposetoryHeaderFormat ReposetoryHeader;
+
 	int LotNr;
 	char *subname;
-	char htmlbuffer[524288];
-	char imagebuffer[524288];
-	char *acl_allow;
-	char *acl_deny;
-	unsigned long int raddress;
-	char path[1024];
-	char path2[1024];
-	char path3[1024];
+
 	
 	if (argc < 3)
 		errx(1, "Usage: ./gcrepo lotnr subname");
@@ -31,38 +19,8 @@ main(int argc, char **argv)
 	LotNr = atoi(argv[1]);
 	subname = argv[2];
 
+	gcrepo(LotNr,subname);
 
-	while (rGetNext(LotNr,&ReposetoryHeader,htmlbuffer,sizeof(htmlbuffer),imagebuffer,&raddress,0,0,subname,&acl_allow,&acl_deny)) {
-		struct DocumentIndexFormat docindex;
-
-		if (!DIRead(&docindex, ReposetoryHeader.DocID, subname)) {
-			fprintf(stderr, "Unable to locate a DI for %d\n", ReposetoryHeader.DocID);
-			continue;
-		}
-
-		//printf("%p\n", docindex.RepositoryPointer);
-		if (raddress == docindex.RepositoryPointer) {
-			unsigned long int offset;
-			offset = rApendPost(&ReposetoryHeader, htmlbuffer, imagebuffer, subname, acl_allow, acl_deny, "repo.wip");
-			docindex.RepositoryPointer = offset;
-			DIWrite(&docindex, ReposetoryHeader.DocID, subname, "DocumentIndex.wip");
-			printf("Writing DocID: %d\n", ReposetoryHeader.DocID);
-		} else {
-			printf("Garbage collecting %d at %lx\n", ReposetoryHeader.DocID, raddress);
-		}
-	}
-
-	/* And we have a race... */
-	GetFilPathForLot(path, LotNr, subname);
-	strcpy(path2, path);
-	strcpy(path3, path);
-	strcat(path, "repo.wip");
-	strcat(path2, "reposetory");
-	rename(path, path2);
-	strcpy(path, path3);
-	strcat(path, "DocumentIndex.wip");
-	strcat(path3, "DocumentIndex");
-	rename(path, path3);
 
 	return 0;
 }
