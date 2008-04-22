a4b38b07180f325926611d13d7c5f7f09fa159f6
Eirik Nygaard <eirikald@pvv.ntnu.no>
2008-04-22 14:32:18 +0000

    Some more output.

--- src/bb-phone-home/bbport.php
+++ src/bb-phone-home/bbport.php
@@ -1,19 +1,18 @@
 <?php
 
 $retval = 0;
-$cmd = "/bin/bb-phone-home-get-port.pl";
-$args = $_GET['hostname'];
+$cmd = "/home/boitho/bb-phone-home-get-port.pl";
+$args = "".$_GET['hostname'] . "-" . $_SERVER['REMOTE_ADDR'] . "";
 
 if (!preg_match('/^[\w.\-\d\/]+$/', $args)) {
 	exit("Illegal hostname");
 }
 
 
-
-$line = system($cmd . " " .$args, $retval);
+$line = system($cmd . " \"$args\"", $retval);
 
 if ($retval != 0) {
-	print "\nSomething went wrong.\n";
+	print "\nAn Error has occured. Please contact support.\n";
 }
 
 
