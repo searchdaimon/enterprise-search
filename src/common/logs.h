e2f02fc7add5d6ab875501bda3bc4e48dc0a2116
Runar Buvik <rb@searchdaimon.com>
2008-04-30 15:16:20 +0000

    Lagt til standar include filer.

--- src/common/logs.h
+++ src/common/logs.h
@@ -1,5 +1,9 @@
 #include <stdarg.h>
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 void blog(FILE *LOG, int level, const char *fmt, ...);
 int openlogs(FILE **LOGACCESS, FILE **LOGERROR, char name[]);
 void bvlog(FILE *LOG, int level,const char *fmt,va_list ap);
