52b7718a9edf2ccb2bbacde8e476a7a0e7802f0f
Eirik Nygaard <eirikald@pvv.ntnu.no>
2008-05-05 14:14:21 +0000

    First stab at SD::Df

---
+++ Modules/SD/Df.pm
@@ -0,0 +1,34 @@
+#!/usr/local/bin/perl
+
+package SD::Df;
+
+use warnings;
+use strict;
+
+sub df($$) {
+	my ($path, $bytes) = @_;
+
+	my $partinfo = `df --block-size=$bytes $path | tail -n1`;
+	chomp($partinfo);
+	print $partinfo . "\n";
+
+	# Parses: /dev/sda5             40305932   3921520  34336932  11% /
+	my ($part, $blocks, $used, $avail, $useper, $mounted) = $partinfo =~ /^([^\s]+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d{1,3})\%\s+(.*)$/;
+
+	return undef unless defined $part and defined $blocks and defined $used and
+	    defined $avail and defined $useper and defined $mounted;
+
+	return {
+		size => $blocks,
+		used => $used,
+		useper => $useper,
+		mounted => $mounted,
+		part => $part
+	};
+}
+
+use Data::Dumper;
+
+print Dumper(df("/boot", 1));
+
+1;
