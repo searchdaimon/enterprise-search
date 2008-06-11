2c7f8586483347ca69a7fafe33350e961f3ec64e
Eirik Nygaard <eirikald@pvv.ntnu.no>
2008-06-11 11:29:04 +0000

    Add hashtable helper functions.

---
+++ src/common/ht.c
@@ -0,0 +1,51 @@
+#include <string.h>
+#include <stdlib.h>
+
+unsigned int
+ht_stringhash(void *ky)
+{
+	char *p = ky;
+	unsigned int hash = 5381;
+	int c;
+
+	while ((c = *p++))
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+
+	return hash;
+}
+
+int
+ht_stringcmp(void *k1, void *k2)
+{
+	char *c1, *c2;
+
+	c1 = k1;
+	c2 = k2;
+
+	return (strcmp(c1, c2) == 0);
+}
+
+unsigned int
+ht_wstringhash(void *ky)
+{
+	wchar_t *p = ky;
+	unsigned int hash = 5381;
+	int c;
+
+	while ((c = *p++))
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+
+	return hash;
+}
+
+int
+ht_wstringcmp(void *k1, void *k2)
+{
+	wchar_t *c1, *c2;
+
+	c1 = k1;
+	c2 = k2;
+
+	return (wcscmp(c1, c2) == 0);
+}
+
---
+++ src/common/ht.h
@@ -0,0 +1,11 @@
+#ifndef _HT_H_
+#define _HT_H_
+
+unsigned int ht_stringhash(void *ky);
+int ht_stringcmp(void *k1, void *k2);
+/* wchar_t */
+unsigned int ht_wstringhash(void *ky);
+int ht_wstringcmp(void *k1, void *k2);
+
+	
+#endif /* _HT_H_ */
