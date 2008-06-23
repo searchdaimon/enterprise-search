4ac59b14021e89fc6fbc6364e64efcaaaf6e9780
Eirik Nygaard <eirikald@pvv.ntnu.no>
2008-06-23 08:19:46 +0000

    A program that gives out the offset of the elements in the repo struct.

---
+++ src/repoReadStandalone/elementoffset.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+#include "common.h"
+
+
+int
+main(void)
+{
+	RepositoryHeader_t h;
+	char *p, *p2;
+#define PRINT_OFFSET(name) p = &h.name; printf("%s offset is: %d and has size: %d\n", #name, p - p2, sizeof(h.name));
+
+	p2 = &h;
+	PRINT_OFFSET(DocID);
+	PRINT_OFFSET(url);
+	PRINT_OFFSET(content_type);
+	PRINT_OFFSET(IPAddress);
+	PRINT_OFFSET(response);
+	PRINT_OFFSET(htmlSize);
+	PRINT_OFFSET(imageSize);
+	PRINT_OFFSET(time);
+	PRINT_OFFSET(userID);
+	PRINT_OFFSET(clientVersion);
+
+	return 0;
+}
