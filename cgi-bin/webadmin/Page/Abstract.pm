0716bde7c416db53a087ec924d1d89c51ae580c2
Dagur Valberg Johannsson <dvj@dagur.com>
2008-06-25 08:26:11 +0000

    *** empty log message ***

--- cgi-bin/webadmin/Page/Abstract.pm
+++ cgi-bin/webadmin/Page/Abstract.pm
@@ -4,25 +4,89 @@ package Page::Abstract;
 use strict;
 use warnings;
 use Carp;
+use Data::Dumper;
+use Sql::Sql;
+use CGI qw(header);
+use CGI::State;
+use Template;
+use Template::Stash;
+use config qw(%CONFIG);
+
+my %DEF_TPL_OPT = (
+    INCLUDE_PATH => './templates:./templates/common',
+    ANYCASE => 1,
+    COMPILE_EXT => 'ctt',
+    COMPILE_DIR => $CONFIG{tpl_tmp},
+);
+my %TPL_SCALAR_FUNC = (
+    escape => sub { "\Q$_[0]\E" }   
+);
+
+my $dbh;
 
 # Constructor: new
 #
 # Attributes:
-#	dbh - Database handler
 #	@ - Anything, sent to the childclass' _init function.
 sub new {
     my $class = shift;
-    my $dbh = shift;
-    
-    my $self = {};
-    bless $self, $class;
+    my $d = $dbh || Sql::Sql->new->get_connection();
 
-    $self->{'dbh'} = $dbh;
+    my $cgi = CGI->new;
+    my $self =  bless {
+        dbh => $d,
+        cgi => $cgi,
+        state => CGI::State->state($cgi),
+    }, $class;
+
     if ($self->can("_init")) { 
-		# initialize if supported
-		$self->_init(@_);
-	}
-	return $self;
+        $self->_init(@_);
+    }
+    return $self;
+}
+
+sub get_dbh { $_[0]->{dbh} }
+sub get_state {  wantarray ? return %{$_[0]->{state}} : $_[0]->{state} }
+sub get_cgi { $_[0]->{cgi} }
+
+sub process_tpl {
+    my ($s, $tpl_file, $vars_ref, %opt) = @_;
+    croak "No tpl_file provided"
+        unless defined $tpl_file;
+
+    # Init tpl
+    while (my ($key, $val) = each %DEF_TPL_OPT) {
+        $opt{$key} = $val 
+            unless defined $opt{$key};
+    }
+    while (my ($name, $sub)  = each %TPL_SCALAR_FUNC) {
+        $Template::Stash::SCALAR_OPS->{$name} = $sub;
+    }
+
+    my @extra_folders;
+    if (ref $opt{tpl_folders} eq 'ARRAY') {
+        @extra_folders = @{$opt{tpl_folders}};
+    }
+    else {
+        @extra_folders = ($opt{tpl_folders})
+            if defined $opt{tpl_folders};
+    }
+    delete $opt{tpl_folders};
+
+    $opt{INCLUDE_PATH} .= join ":./templates/", ('', @extra_folders)
+        if scalar @extra_folders;
+
+    # Print html.
+    my $no_header = $opt{no_header} and delete $opt{no_header};
+
+
+    my $tpl = Template->new(%opt);
+    print header(-type => "text/html", -charset => "UTF-8")
+        unless $no_header;
+    $tpl->process($tpl_file, $vars_ref)
+        or croak "Template '$tpl_file' error: ", $tpl->error();
+    
+    1;
 }
 
 1;
