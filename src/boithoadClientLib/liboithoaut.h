688926304048c35ca84a94e85fb094fd49b398c9
Eirik Nygaard <eirikald@pvv.ntnu.no>
2008-07-01 09:38:25 +0000

    Prototype for boithoad_listMailUsers

--- src/boithoadClientLib/liboithoaut.h
+++ src/boithoadClientLib/liboithoaut.h
@@ -1,5 +1,6 @@
 int boitho_authenticat(const char username_in[],char password_in[]);
 int boithoad_listUsers(char **respons_list[],int *nrofresponses);
+int boithoad_listMailUsers(char **respons_list[],int *nrofresponses);
 int boithoad_groupsForUser(const char username_in[],char **respons_list[],int *nrofresponses);
 void boithoad_respons_list_free(char *respons_list[]);
 int boithoad_listGroups(char **respons_list[],int *nrofresponses);
