2a8ef56fe84c759becc622ef4a761c56eb867932
Dagur Valberg Johannsson <dvj@dagur.com>
2008-10-23 13:31:27 +0000

    tidsbegrense lesing + signering av tidsparam

--- src/ShowCache2/main.c
+++ src/ShowCache2/main.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 #include "../cgi-util/cgi-util.h"
 
 
@@ -8,6 +9,10 @@
 #include "../common/lot.h"
 #include "../common/reposetory.h"
 #include "../common/DocumentIndex.h"
+
+#ifdef BLACK_BOKS
+	#define URL_EXPIRE_TIME 3600
+#endif
 
 int main(int argc, char *argv[]) {
 
@@ -23,8 +28,14 @@ int main(int argc, char *argv[]) {
 	char *attributes;
 	char *url;
 
-	int iDocID;
+	docid iDocID;
 	char subname[maxSubnameLength];
+
+#ifdef BLACK_BOKS
+	bool validate_url = true;
+	unsigned int url_time;
+	unsigned int url_signature;
+#endif
 	
         if (getenv("QUERY_STRING") != NULL) {
         
@@ -38,34 +49,70 @@ int main(int argc, char *argv[]) {
         	        exit(0);
         	}
 
-        	if (cgi_getentrystr("P") == NULL) {
-        	        perror("Did'n receive any query.");
-        	}
+		iDocID = cgi_getentryunsignedint("D");
+		if (cgi_errno == CGIERR_NOT_UNSIGNED_INTEGER)
+			errx(1, "Parameter D missing/invalid");
 
-		iDocID = cgi_getentryint("D");
-		strncpy(subname,cgi_getentrystr("subname"),sizeof(subname) -1);
+		
+		const char * query_subname = cgi_getentrystr("subname");
+		if (cgi_errno != CGIERR_NONE)
+			errx(1, "Error with parameter subname");
+
+		strncpy(subname, query_subname ,sizeof(subname) -1);
+
+#ifdef BLACK_BOKS
+		url_time = cgi_getentryunsignedint("time");
+		if (cgi_errno == CGIERR_NOT_UNSIGNED_INTEGER)
+			errx(1, "Parameter time missing/invalid");
+
+		url_signature = cgi_getentryunsignedint("sign");
+		if (cgi_errno == CGIERR_NOT_UNSIGNED_INTEGER)
+			errx(1, "Parameter sign missing/invalid");
+#endif
+
 
 	}
 	else if (argc > 2) {
 		//iPointer = 9695;
 		//iSize = 2288;
 		//LotNr = 14;
+#ifdef BLACK_BOKS
+		validate_url = false;
+#endif
 
 		iDocID = atoi(argv[1]); 	// D
 		strncpy(subname,argv[2],sizeof(subname) -1);
 
 	}
 	else {
-		printf("no query given. \nUsage: DocID Pointer Size subname\n");
+		printf("no query given. \nUsage: DocID subname\n");
 		exit(1);
 	}
 
+
+#ifdef BLACK_BOKS
+	if (validate_url) {
+		unsigned int our_sign = sign_cache_params(iDocID, subname, url_time);
+
+		if (url_signature != our_sign)
+			errx(1, "Data signature did not match provided signature");
+	
+
+		time_t t = time(NULL);
+		if (url_time + URL_EXPIRE_TIME < t)
+			errx(1, "URL has expired.");
+	}
+#endif
+	
 	printf("Content-type: text/html\n\n");
 
 	struct DocumentIndexFormat DocumentIndexPost;
 
 	DIRead_fmode (&DocumentIndexPost,iDocID,subname, 's');
 
+//#ifdef BLACK_BOKS
+	DocumentIndexPost.imageSize = 0;
+//#endif
 
 
 	htmlBufSize = (DocumentIndexPost.htmlSize * 30);
