208b44bb51f4f74ccb952d8d3a00fa01a799de68
Eirik Nygaard <eirikald@pvv.ntnu.no>
2008-10-30 13:03:42 +0000

    Update sql update script to match new release. Still missing some config entries.

--- sql/update-cur.sql
+++ sql/update-cur.sql
@@ -48,3 +48,61 @@ INSERT INTO connectors VALUES (64,'Intranet',NULL,NULL,NULL,NULL,0,'custom_param
 INSERT INTO param VALUES (102,64,'isPasswordProtected','1 = user must be able to log in, 0 = skip online password check.');
 INSERT INTO param VALUES (101,64,'url','http://www.example.com');
 
+
+-- New usersystem handling
+
+CREATE TABLE system (
+  id int(10) unsigned NOT NULL auto_increment,
+  name varchar(255) NOT NULL default 'Unnamed system',
+  ip varchar(32) default NULL,
+  user varchar(255) default NULL,
+  password varchar(255) default NULL,
+  is_primary tinyint(4) default '0',
+  connector int(10) unsigned default NULL,
+  PRIMARY KEY  (id)
+) TYPE=MyISAM COMMENT='Brukersystem';
+
+
+CREATE TABLE systemMapping (
+  system int(10) unsigned NOT NULL default '0',
+  prim_usr varchar(245) NOT NULL default '',
+  secnd_usr varchar(245) NOT NULL default '',
+  PRIMARY KEY  (system,prim_usr,secnd_usr)
+) TYPE=MyISAM COMMENT='Mapping secondary systems to primary';
+
+
+CREATE TABLE systemParam (
+  connector int(11) unsigned NOT NULL default '0',
+  param varchar(255) NOT NULL default '',
+  note varchar(255) default NULL,
+  PRIMARY KEY  (connector,param),
+  UNIQUE KEY systemParam (connector,param)
+) TYPE=MyISAM;
+
+
+CREATE TABLE systemParamValue (
+  param varchar(255) NOT NULL default '',
+  system int(10) unsigned NOT NULL default '0',
+  value varchar(255) default NULL,
+  PRIMARY KEY  (param,system)
+) TYPE=MyISAM;
+
+CREATE TABLE foreignUserSystem (
+  usersystem int(11) NOT NULL default '0',
+  username varchar(255) NOT NULL default '',
+  groupname varchar(255) NOT NULL default ''
+) TYPE=MyISAM;
+
+
+-- Active directory system
+INSERT INTO system (id, name, ip, user, password, is_primary, connector) SELECT 1 AS id, 'Active Directory' AS name, c3.configvalue AS ip, c1.configvalue AS user, c2.configvalue AS password, 1 AS is_primary, 1 AS connector FROM config AS c1, config AS c2, config AS c3 WHERE c1.configkey = 'msad_user' AND c2.configkey = 'msad_password' AND c3.configkey = 'msad_ip';
+
+INSERT INTO systemParam VALUES (1,'ldapstring',NULL);
+INSERT INTO systemParam VALUES (1,'domain',NULL);
+INSERT INTO systemParam VALUES (1,'ldapbase',NULL);
+INSERT INTO systemParam VALUES (1,'ldapgroupstring',NULL);
+
+INSERT INTO systemParamValue (param, system, value) SELECT 'domain' as param, 1 AS system, configvalue AS value FROM config WHERE config.configkey = 'msad_domain'; 
+INSERT INTO systemParamValue (param, system, value) VALUES('ldapstring', 1, '');
+INSERT INTO systemParamValue (param, system, value) VALUES('ldapbase', 1, '');
+INSERT INTO systemParamValue (param, system, value) VALUES('ldapgroupstring', 1, '');
