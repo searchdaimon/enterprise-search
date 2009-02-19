cbb63c7e0a1769fdbe1afe7fe37ffdc2412d31e9
Eirik Nygaard <eirikald@pvv.ntnu.no>
2009-02-19 13:29:20 +0000

    System key.

---
+++ src/common/key.c
@@ -0,0 +1,51 @@
+#include <mysql.h>
+#include <err.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "key.h"
+
+
+/* MYSQL login information */
+#define MYSQL_HOST	"localhost"
+#define MYSQL_USER	"boitho"
+#define MYSQL_PASS	"G7J7v5L5Y7"
+#define MYSQL_DB	"boithobb"
+
+int
+key_get(char *keyout)
+{
+	MYSQL db;
+	MYSQL_ROW row;
+	MYSQL_RES *res;
+	size_t qlen;
+	char query[2048];
+
+	if (!mysql_init(&db))
+		errx(1, "mysql_init error");
+
+	if (!mysql_real_connect(&db, MYSQL_HOST, MYSQL_USER, MYSQL_PASS, MYSQL_DB, 3306, NULL, 0))
+		errx(1, "Unable to connect to mysql: %s", mysql_error(&db));
+
+	qlen = snprintf(query, sizeof(query), "SELECT configvalue FROM config WHERE configkey = 'key'");
+
+	if (mysql_real_query(&db, query, qlen))
+		errx(1, "Unable to perform query: %s: %s", query, mysql_error(&db));
+
+	res = mysql_store_result(&db);
+	row = mysql_fetch_row(res);
+	if (row == NULL || mysql_num_rows(res) < 1)
+		errx(1, "No key found");
+		
+	strcpy(keyout, row[0]);
+
+	mysql_close(&db);
+
+	return 1;
+}
+
+int
+key_equal(char *k1, char *k2)
+{
+	return (strcmp(k1, k2) == 0);
+}
---
+++ src/common/key.h
@@ -0,0 +1,10 @@
+#ifndef _COMMON_KEY_H_
+#define _COMMON_KEY_H_
+
+#define KEY_LEN 32
+#define KEY_STR_LEN (KEY_LEN+1)
+
+int key_get(char *keyout);
+int key_equal(char *k1, char *k2);
+
+#endif
