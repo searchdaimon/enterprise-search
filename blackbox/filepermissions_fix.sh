1c9b6eb65885c74556ecec949b136087680363d1
Eirik Nygaard <eirikald@pvv.ntnu.no>
2009-03-25 15:08:00 +0000

    bbdocumetnwebadd log

--- blackbox/filepermissions_fix.sh
+++ blackbox/filepermissions_fix.sh
@@ -4,10 +4,13 @@ chown boitho:boitho /home/boitho/boithoTools/var
 chown boitho:boitho /home/boitho/boithoTools/var/*
 chown boitho:boitho /home/boitho/boithoTools/logs
 chown boitho:boitho /home/boitho/boithoTools/logs/*
+if [ ! -f /home/boitho/boithoTools/logs/bbdocumentWebAdd.log ]; then
+	touch /home/boitho/boithoTools/logs/bbdocumentWebAdd.log
+fi
+chown apache /home/boitho/boithoTools/logs/bbdocumentWebAdd.log
 
 chown -R boitho:boitho /boithoData/lot/
 
 chown boitho:boitho /home/boitho/boithoTools/data/dict/
 chown apache:apache /home/boitho/boithoTools/cgi-bin/webadmin/.htpasswd
 chown apache:apache /home/boitho/boithoTools/crawlers
-
