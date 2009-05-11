3be4b5afe70bc3f592ab2fea3b6acca7c1c91d6f
Eirik Nygaard <eirikald@pvv.ntnu.no>
2009-05-11 12:01:30 +0000

    Handle larger amounts of files. And update to newest wikidump module.

--- perl/datadumps/wikipediaDumpAllText.pl
+++ perl/datadumps/wikipediaDumpAllText.pl
@@ -1,4 +1,5 @@
   
+use File::Path;
 #use utf8;
 #binmode STDOUT, ":utf8";
 
@@ -16,6 +17,8 @@ my $pages = Parse::MediaWikiDump::Pages->new($file);
 my $page;
   
 
+$|++;
+
 
 
 
@@ -24,100 +27,106 @@ my $page;
 
 my $openlist = 0;
 my $count = 0;
-while(defined($page = $pages->page)) {
+while(defined($page = $pages->next)) {
 
 
-   
-    #main namespace only 
-    #	print "namespace: " . $page->namespace . "\n";          
-    next unless $page->namespace eq '';
-    #	print "categories: ". $page->categories ."\n";
-    #print $count, ": ", $page->title, "\n" unless defined($page->categories);
 
-  	#if (!defined($page->categories)) {
-		my $namespace = $page->namespace;
-		my $id = $page->id;
-        my $title = $page->title;
-        my $text = $page->text;
-		
-		#$title = from_utf8(-string =>$title, -charset => 'ISO-8859-1');
+#main namespace only 
+#	print "namespace: " . $page->namespace . "\n";          
+	next unless $page->namespace eq '';
+#	print "categories: ". $page->categories ."\n";
+#print $count, ": ", $page->title, "\n" unless defined($page->categories);
 
-		#fjerner ting som ikke er lov i filnavnet
-		$filename = $title;
-		$filename =~ s/[^a-zA-Z0-9 ]/_/g;
+#if (!defined($page->categories)) {
+	my $namespace = $page->namespace;
+	my $id = $page->id;
+	my $title = $page->title;
+	my $text = $page->text;
 
-		my $dir = $filename;
-		$dir =~ s/[^a-zA-Z0-9]//g;
-		$dir =~ s/(..)/xx/;
-		$dir = $1;
-		$dir = lc($dir);
-		$dir = $maindir . "/" . $dir;
-		$filename = $dir . "/" . $filename . ".txt";
+#$title = from_utf8(-string =>$title, -charset => 'ISO-8859-1');
 
-		
+#fjerner ting som ikke er lov i filnavnet
+	$filename = $title;
+	$filename =~ s/[^a-zA-Z0-9 ]/_/g;
+
+	my $dir = $filename;
+	$dir =~ s/[^a-zA-Z0-9]//g;
+#	$dir =~ s/(..)/xx/;
+
+#	next unless (length($dir) < 4);
+
+	$dir =~ /(.)(.)?(.)?(.)?/;
+	my ($a, $b, $c, $d) = ($1, $2, $3, $4);
+
+#	print "$a $b $c $d\n";
+
+
+#	$dir = $1;
+	$a = lc($a);
+	$b = lc($b);
+	$c = lc($c);
+	$d = lc($d);
+	$dir = $maindir . "/$a";
+	$dir .= "/$b" if defined $b;
+	$dir .= "/$c" if defined $c;
+	$dir .= "/$d" if defined $d;
+	mkpath($dir) unless (-d $dir);
+	$filename = $dir . "/" . $filename . ".txt";
+
 #		print $count . ' '. $filename . ', len: ' . length($$text) . "\n";
-		
-		if (!(-d $dir)) {
-			system("mkdir -p $dir");
-		}
 
-		if (-e $filename) {
-			print "file $filename exists\n";
-			next;
-		}
-		
-		eval {
-			local $SIG{ALRM} = sub { die "alarm\n" };       # NB \n required
-	        	alarm $timeout;
+	if (-e $filename) {
+		print STDERR "file exists: $filename\n";
+		next;
+	}
 
-		
-		
+	eval {
+		local $SIG{ALRM} = sub { die "alarm\n" };       # NB \n required
+			alarm $timeout;
 
-		
 
-		
-			my @lines = split(/\n/,$$text);
-	
-			my $text = '';
 
-			foreach my $i (@lines) {
-				$i =~ s/\[\[([^\|]+\|)([^\]]+)\]\]/$2/g;
-				#honterer linker med annen link en beskrivelse
-				$i =~ s/\[\[([^\]]+)\]\]/$1/g;
-			
-				#honterer linker
-				$i =~ s/\[\[([^\]]+)\]\]/$1/g;
-			
-				#seltter overskrift makup
-				$i =~ s/'//g;
-			
-				$i =~ s/=//g;
-				$text .= $i . "\n";				
+		my @lines = split(/\n/,$$text);
+
+		my $text = '';
+
+		foreach my $i (@lines) {
+			$i =~ s/\[\[([^\|]+\|)([^\]]+)\]\]/$2/g;
+			#honterer linker med annen link en beskrivelse
+			$i =~ s/\[\[([^\]]+)\]\]/$1/g;
+
+			#honterer linker
+			$i =~ s/\[\[([^\]]+)\]\]/$1/g;
+
+			#seltter overskrift makup
+			$i =~ s/'//g;
+
+			$i =~ s/=//g;
+			$text .= $i . "\n";				
 #				print $i, "\n";
 
-			} #/foreach
-		
-			open(OUT,">" . $filename) or die("Can't write $filename: $!");
-			binmode(OUT, ':utf8');
-			print OUT $text, "\n";
-			close(OUT);
-		
+		} #/foreach
 
+		open(OUT,">" . $filename) or die("Can't write $filename: $!");
+		binmode(OUT, ':utf8');
+		print OUT $text, "\n";
+		close(OUT);
 
-			
-			alarm 0;
-		};
-		die if $@ && $@ ne "alarm\n";       # propagate errors
-    	if ($@) {
-        	warn("did time out.");
-    	}
+		alarm 0;
+	};
+	die if $@ && $@ ne "alarm\n";       # propagate errors
+		if ($@) {
+			warn("did time out.");
+		}
 
-		++$count;
-	#}
+	++$count;
+#}
 
 	if (($count % 1000) == 0) {
-		print "$count\n";
+		print "$count\r";
 	}
 }
+
+print "\n";
 
 
