84e2c4728906923e8878aedd1fc981a902624c43
cvsmerge <boitho@searchdaimon.com>
2009-08-24 11:26:04 +0000

    Lagt til closelogs() til .h filen.

--- src/common/logs.h
+++ src/common/logs.h
@@ -7,3 +7,4 @@
 void blog(FILE *LOG, int level, const char *fmt, ...);
 int openlogs(FILE **LOGACCESS, FILE **LOGERROR, char name[]);
 void bvlog(FILE *LOG, int level,const char *fmt,va_list ap);
+void closelogs(FILE *LOGACCESS, FILE *LOGERROR);
