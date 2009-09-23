690b30f85850cb80c60b4c4fec561086f2f772ab
cvsmerge <boitho@searchdaimon.com>
2009-09-23 10:57:17 +0000

    #ifdef'et ut dorank().

--- src/searchkernel/searchkernel.h
+++ src/searchkernel/searchkernel.h
@@ -47,9 +47,12 @@ int dosearch(char query[], int queryLen, struct SiderFormat **Sider, struct Side
 #define RANK_TYPE_FIND	1
 #define RANK_TYPE_SUM	2
 
+#if 0
 int dorank(char query[], int queryLen, struct SiderFormat **Sider, struct SiderHederFormat *SiderHeder,
 char *hiliteQuery, char servername[], struct subnamesFormat subnames[], int nrOfSubnames,
 int MaxsHits, int start, int filterOn, char languageFilter[],char orderby[],int dates[],
 char search_user[],struct filtersFormat *filters,
 	struct searchd_configFORMAT *searchd_config, char *errorstr,int *errorLen, struct iintegerMemArrayFormat *DomainIDs,
 	int, unsigned int, int *);
+#endif
+
