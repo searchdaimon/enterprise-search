e451698ab0039efc191f9fff5c8effcdc315ab7c
cvsmerge <boitho@searchdaimon.com>
2012-11-24 13:08:31 +0000

    Added option to print out html and acl. Restructured some code.

--- src/rread/rread.c
+++ src/rread/rread.c
@@ -5,26 +5,11 @@
 #include <stdlib.h>
 #include <string.h>
 
+void dolot(int LotNr, char *subname, int optPrintHtml, int optPrintAcl, int optStatistics, char *optReponame);
+
 main (int argc, char *argv[]) {
-	int LotNr;
-	char lotPath[255];
 
-	struct ReposetoryHeaderFormat ReposetoryHeader;
-	unsigned long int radress;
-
-	char htmlbuffer[524288];
-	char htmlbuffer_uncom[524288];
-	char imagebuffer[524288];
-	char *acl_allow;
-	char *acl_deny;
-	char *url, *attributes;
-
-	int StatisticsUncompressError = 0;;
-	int StatisticsUncompressOk = 0;
-
-	char uncompresshtml[50000];
-	int uncompresshtmlLength;
-	int nerror;
+	int LotNr, i;
 
 	if (argc < 3) {
 		printf("Error ingen lotnr spesifisert.\n\nEksempel på bruk for å lese lot 2:\n\trread 2 www\n");
@@ -32,20 +17,22 @@ main (int argc, char *argv[]) {
 	}
 
 	int optPrintHtml = 0;
+	int optPrintAcl = 0;
 	int optStatistics = 0;
 	char *optReponame = NULL;
         extern char *optarg;
         extern int optind, opterr, optopt;
         char c;
-        while ((c=getopt(argc,argv,"hsr:"))!=-1) {
+        while ((c=getopt(argc,argv,"hsr:a"))!=-1) {
                 switch (c) {
                         case 'h':
                                 optPrintHtml = 1;
-                                printf("will print html\n");
+                                break;
+                        case 'a':
+                                optPrintAcl = 1;
                                 break;
                         case 's':
                                 optStatistics  = 1;
-                                printf("will statistics\n");
                                 break;
                         case 'r':
 				optReponame = optarg;
@@ -61,35 +48,75 @@ main (int argc, char *argv[]) {
 	LotNr = atoi(argv[1 + optind]);
 	char *subname = argv[2 + optind];
 
+	if (LotNr == 0) {
+		for (i=0;i<maxLots;i++) {
+			dolot(i, 	subname, optPrintHtml, optPrintAcl, optStatistics, optReponame);
+		}
+	}
+	else {
+			dolot(LotNr, 	subname, optPrintHtml, optPrintAcl, optStatistics, optReponame);
+	}
+}
+
+void dolot(int LotNr, char *subname, int optPrintHtml, int optPrintAcl, int optStatistics, char *optReponame) {
+
+
+
+	char lotPath[255];
+
+	struct ReposetoryHeaderFormat ReposetoryHeader;
+	unsigned long int radress;
+
+	char htmlbuffer[524288];
+	char htmlbuffer_uncom[524288];
+	char imagebuffer[524288];
+	char *acl_allow;
+	char *acl_deny;
+	char *url, *attributes;
+
+	int StatisticsUncompressError = 0;;
+	int StatisticsUncompressOk = 0;
+
+	char uncompresshtml[500000];
+	int uncompresshtmlLength;
+	int nerror;
+
 	if (optReponame == NULL) {
 		optReponame = strdup("reposetory");
 	}
 
-	printf("lotnr %i\n",LotNr);
 
 	GetFilPathForLot(lotPath,LotNr,subname);
-	printf("Opning lot at: %s for %s\n",lotPath,subname);
 
 
 	//loppergjenom alle
 	while (rGetNext_reponame(LotNr,&ReposetoryHeader,htmlbuffer,sizeof(htmlbuffer),imagebuffer,&radress,0,0,subname,&acl_allow,&acl_deny,optReponame, &url, &attributes)) {
 
-		printf("DocId: %i url: %s res %hi htmlsize %hi time %lu, radress %lu\n",ReposetoryHeader.DocID,ReposetoryHeader.url,ReposetoryHeader.response,ReposetoryHeader.htmlSize,ReposetoryHeader.time,radress);
+
+		printf("DocId: %i, url: %s, res: %hi, htmlsize: %hi, time: %lu, radress: %lu",ReposetoryHeader.DocID,ReposetoryHeader.url,ReposetoryHeader.response,ReposetoryHeader.htmlSize2,ReposetoryHeader.time,radress);
 		uncompresshtmlLength = sizeof(uncompresshtml);
-		if ( (nerror = uncompress((Bytef*)uncompresshtml,(uLong *)&uncompresshtmlLength,(Bytef*)htmlbuffer,ReposetoryHeader.htmlSize)) != 0) {
+		if ( (nerror = uncompress((Bytef*)uncompresshtml,(uLong *)&uncompresshtmlLength,(Bytef*)htmlbuffer,ReposetoryHeader.htmlSize2)) != 0) {
                 	printf("uncompress error. Code: %i\n",nerror);
                 
                         continue;
                 }
 
-		if (optPrintHtml || optStatistics) {
-			if (runpack(htmlbuffer_uncom,sizeof(htmlbuffer_uncom),htmlbuffer,ReposetoryHeader.htmlSize)) {
-				if (optPrintHtml) {
-					printf("################################\n%s##############################\n",htmlbuffer_uncom);
+
+		if (optPrintHtml || optStatistics || optPrintAcl) {
+			if (runpack(htmlbuffer_uncom,sizeof(htmlbuffer_uncom),htmlbuffer,ReposetoryHeader.htmlSize2)) {
+
+				if (optPrintAcl && !optPrintHtml) {
+					printf(", acl_allow: \"%s\"",acl_allow);
+					printf(", acl_deny: \"%s\"",acl_deny);
 				}
 
-				printf("acl_allow: \"%s\"\n",acl_allow);
-				printf("acl_deny: \"%s\"\n",acl_deny);
+				if (optPrintHtml) {
+					printf("################################\n%s##############################\n",htmlbuffer_uncom);
+					printf("acl_allow: \"%s\"\n",acl_allow);
+					printf("acl_deny: \"%s\"\n",acl_deny);
+
+				}
+
 
 				++StatisticsUncompressOk;
 			}
@@ -99,8 +126,13 @@ main (int argc, char *argv[]) {
 				}
 				++StatisticsUncompressError;
 			}
-			printf("Attributes: %s\n", attributes);
+
+			if (optPrintHtml) {
+				printf("Attributes: %s\n", attributes);
+			}
 		}
+
+		printf("\n");
 
 	}
 	
