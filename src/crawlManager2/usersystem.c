e5e31aaf2ce8a865c616445553a02a4f0d55e1bb
cvsmerge <boitho@searchdaimon.com>
2013-02-18 11:22:58 +0000

    Changes to the interface 2009-2011.

--- src/crawlManager2/usersystem.c
+++ src/crawlManager2/usersystem.c
@@ -1,34 +1,40 @@
 #include <stdio.h>
+#include <EXTERN.h>
+#include <perl.h>
 
 #include "usersystem.h"
 #include "../perlembed/perlembed.h"
+#include "../crawl/crawl.h"
 
-#define PERL_LIST_GROUPS "PerlUserSystem::_internal_list_groups"
-#define PERL_LIST_USERS "PerlUserSystem::_internal_list_users"
+#define PERL_LIST_GROUPS "PerlUserSystem::list_groups"
+#define PERL_LIST_USERS "PerlUserSystem::list_users"
 #define PERL_GET_NAME "PerlUserSystem::_internal_get_name"
-#define PERL_AUTH_USER "PerlUserSystem::_internal_authenticate"
+#define PERL_AUTH_USER "PerlUserSystem::authenticate"
+
 
 int
 us_authenticate_perl(usersystem_data_t *data, char *user, char *password)
 {
 	usersystem_perl_t *usp = data->usc->usersystem.us_perl;
 	HV *params = newHV();
-	SV *in_user = newSVpv(user, strlen(user));
-	SV *in_pass = newSVpv(password, strlen(password));
-	SV *out_retval = newSViv(-1);
-	
+	int ret;	
+	struct us_cargsF cargs;
+
+
+        HV *obj_attr = newHV();
+        hv_store(obj_attr, "ptr", strlen("ptr"), sv_2mortal(newSViv(PTR2IV(&cargs))), 0);
+
+
 	ht_to_perl_ht(params, data->parameters);
 
-	hv_store(params, "_in_user", strlen("_in_user"), sv_2mortal(newRV((SV *)in_user)), 0);
-	hv_store(params, "_in_pass", strlen("_in_pass"), sv_2mortal(newRV((SV *)in_pass)), 0);
-	hv_store(params, "_out_retval", strlen("_out_retval"), sv_2mortal(newRV((SV *)out_retval)), 0);
-	if (!perl_embed_run(usp->perlpath, PERL_AUTH_USER, params, NULL, NULL, NULL, 0)) {
-		warnx("perl error");
-		return 0;
-	}
-	int ret = SvIV(out_retval);
+	hv_store(params, "Enduser", strlen("Enduser"), sv_2mortal(newSVpv(user, 0)), 0);
+	hv_store(params, "Endpassword", strlen("Endpassword"), sv_2mortal(newSVpv(password, 0)), 0);
+
+
+	ret = perl_embed_run(usp->perlpath, PERL_AUTH_USER, params, "PerlUserSystem", obj_attr, NULL, 0);
+
 	if (ret == -1) {
-		warnx("no return value from perl (auth)");
+		warnx("perl error");
 		return 0;
 	}
 
@@ -38,42 +44,29 @@ us_authenticate_perl(usersystem_data_t *data, char *user, char *password)
 int
 us_listUsers_perl(usersystem_data_t *data, char ***users, int *n_users)
 {
+
 	usersystem_perl_t *usp = data->usc->usersystem.us_perl;
-	AV *av = newAV();
 	HV *params = newHV();
+	struct us_cargsF cargs;
 	int i;
+
+
+        HV *obj_attr = newHV();
+        hv_store(obj_attr, "ptr", strlen("ptr"), sv_2mortal(newSViv(PTR2IV(&cargs))), 0);
 	
-	av_store( av, 0, &PL_sv_undef );
 	ht_to_perl_ht(params, data->parameters);
 
-	hv_store(params, "_internal_users", strlen("_internal_users"), sv_2mortal(newRV((SV *)av)), 0);
-	if (!perl_embed_run(usp->perlpath, PERL_LIST_USERS, params, NULL, NULL, NULL, 0)) {
+	if (perl_embed_run_arr(usp->perlpath, PERL_LIST_USERS, params, "PerlUserSystem", obj_attr, NULL, 0, users, n_users) == -1) {
 		warnx("perl error");
+		*n_users = 0;
+		*users = NULL;
 		return 0;
 	}
 
-	if (av_len(av) == -1) { /* No users */
-		*n_users = 0;
-		*users = NULL;
-		return 1;
-	}
+	printf(" n_users=%d\n", n_users);
 
-	*n_users = av_len(av)+1;
-	*users = malloc(((*n_users)+1) * sizeof(char*));
-	i = 0;
-	while (av_len(av) != -1) {
-		SV *user = av_pop(av);
-		STRLEN data_size;
-		char *suser;
-		
-		suser = SvPV(user, data_size);
-		(*users)[i] = strdup(suser);
-		i++;
-	}
-	(*users)[i] = NULL;
-
-			
 	return 1;
+
 }
 
 
@@ -81,42 +74,25 @@ int
 us_listGroupsForUser_perl(usersystem_data_t *data, const char *user, char ***groups, int *n_groups)
 {
 	usersystem_perl_t *usp = data->usc->usersystem.us_perl;
-	AV *av = newAV();
 	HV *params = newHV();
-	SV *username = newSVpv(user, strlen(user));
+	struct us_cargsF cargs;
 	int i;
-	
-	av_store( av, 0, &PL_sv_undef );
-	ht_to_perl_ht(params, data->parameters);
 
-	hv_store(params, "_internal_groups", strlen("_internal_groups"), sv_2mortal(newRV((SV *)av)), 0);
-	hv_store(params, "_in_username", strlen("_in_username"), sv_2mortal(newRV((SV *)username)), 0);
-	if (!perl_embed_run(usp->perlpath, PERL_LIST_GROUPS, params, NULL, NULL, NULL, 0)) {
+
+        HV *obj_attr = newHV();
+        hv_store(obj_attr, "ptr", strlen("ptr"), sv_2mortal(newSViv(PTR2IV(&cargs))), 0);
+	
+	ht_to_perl_ht(params, data->parameters);
+	hv_store(params, "Enduser", strlen("Enduser"), sv_2mortal(newSVpv(user, 0)), 0);
+
+	if (perl_embed_run_arr(usp->perlpath, PERL_LIST_GROUPS, params, "PerlUserSystem", obj_attr, NULL, 0, groups, n_groups) == -1) {
 		warnx("perl error");
 		*n_groups = 0;
 		*groups = NULL;
 		return 0;
 	}
 
-	if (av_len(av) == -1) { /* No groups */
-		*n_groups = 0;
-		*groups = NULL;
-		return 1;
-	}
-
-	*n_groups = av_len(av)+1;
-	*groups = malloc(((*n_groups)+1) * sizeof(char*));
-	i = 0;
-	while (av_len(av) != -1) {
-		SV *group = av_pop(av);
-		STRLEN data_size;
-		char *sgroup;
-		
-		sgroup = SvPV(group, data_size);
-		(*groups)[i] = strdup(sgroup);
-		i++;
-	}
-	(*groups)[i] = NULL;
+	printf(" n_groups=%d\n", *n_groups);
 
 	return 1;
 }
