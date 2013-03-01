e20a47571d2ac414ee50523e6e520e586d501d6b
cvsmerge <boitho@searchdaimon.com>
2013-03-01 13:04:54 +0000

    Added support for converting more than one document per run.

--- src/bbdocumentConvertTest/main.c
+++ src/bbdocumentConvertTest/main.c
@@ -8,8 +8,13 @@
 #include "../bbdocument/bbdocument.h"
 #include "../common/bfileutil.h"
 #include "../common/bprint.h"
+#include "../common/define.h"
+
+int global_bbdnport;
+
 
 main (int argc, char *argv[]) {
+
 
 	FILE *fh;
 	struct stat inode;      // lager en struktur for fstat å returnere.
@@ -18,76 +23,71 @@ main (int argc, char *argv[]) {
 	char *documentfinishedbuf;
 	int documentfinishedbufsize;
 	buffer *buffer;
+	char *filname;
+	int i;
 
-	if (argc != 2) {
+	if (argc < 2) {
 		printf("usage ./%s doc\n",argv[0]);
 		exit(1);
 	}
 
-	char *filname = argv[1];
 	bbdocument_init(NULL);
 
-	if ((fh = fopen(filname,"rb")) == NULL){
-		perror(filname);
-		exit(1);
-	}
-	fstat(fileno(fh),&inode);
-	data = malloc(inode.st_size +1);
-	fread(data,1,inode.st_size,fh);
-	fclose(fh);
+	for (i=1;i<argc;i++) {
 
-	if ((documenttype_real = sfindductype(filname)) == NULL) {
-                        printf("can't add type because I cant decide format. File name isent dos type (8.3).\n");
-                        exit(1);;
-        }
-
-	documentfinishedbufsize = ((inode.st_size *2) +512);
-	documentfinishedbuf = malloc(documentfinishedbufsize);
+		filname = argv[i];
+		printf("Doing \"%s\"\n",filname);
 
 
-	unsigned int lastmodified;
-	char acl_allow[] = "";
-	char acl_denied[] = "";
+		if ((fh = fopen(filname,"rb")) == NULL){
+			perror(filname);
+			exit(1);
+		}
+		fstat(fileno(fh),&inode);
+		data = malloc(inode.st_size +1);
+		fread(data,1,inode.st_size,fh);
+		fclose(fh);
 
-	buffer = buffer_init(0);
+		if ((documenttype_real = sfindductype(filname)) == NULL) {
+	                        printf("can't add type because I cant decide format. File name isent dos type (8.3).\n");
+	                        exit(1);;
+	        }
 
-/*
-int bbdocument_convert(
-                        char filetype[],
-                        char document[],
-                        const int dokument_size,
-                        char **documentfinishedbuf,
-                        int *documentfinishedbufsize,
-                        const char titlefromadd[],
-                        char *subname,
-                        char *documenturi,
-                        unsigned int lastmodified,
-                        char *acl_allow,
-                        char *acl_denied,
-                        char *doctype
-                );
-*/
-	if (!bbdocument_convert(
-			documenttype_real,
-			data,
-			inode.st_size,
-			buffer,
-			"test title",
-			"", //subname
-			filname,
-			lastmodified,
-			acl_allow,
-			acl_denied,
-			NULL
-			)
-		) {
-		printf("cant convert\n");
-		exit(1);
-	}
+		documentfinishedbufsize = ((inode.st_size *2) +512);
+		documentfinishedbuf = malloc(documentfinishedbufsize);
+
+
+		unsigned int lastmodified;
+		char acl_allow[] = "";
+		char acl_denied[] = "";
+
+		buffer = buffer_init(0);
+
+		if (!bbdocument_convert(
+				documenttype_real,
+				data,
+				inode.st_size,
+				buffer,
+				"Test title", //title
+				"", //subname
+				filname,
+				lastmodified,
+				acl_allow,
+				acl_denied,
+				NULL
+				)
+			) {
+			printf("cant convert\n");
+			exit(1);
+		}
 	
 
-	documentfinishedbufsize = buffer_length(buffer);
-	documentfinishedbuf = buffer_exit(buffer);
-	printf("convertet to %ib\nDocument:\n%s\n",documentfinishedbufsize,documentfinishedbuf);
+		documentfinishedbufsize = buffer_length(buffer);
+		documentfinishedbuf = buffer_exit(buffer);
+
+	//temp	printf("convertet to %ib\nDocument:\n%s\n",documentfinishedbufsize,documentfinishedbuf);
+
+
+	}
 
 }
