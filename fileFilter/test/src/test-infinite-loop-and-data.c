a09e38a021247bf5ca3ccafcfce25c78bb91171e
Runar Buvik <rb@searchdaimon.com>
2013-08-06 00:12:17 +0200

    New file filter specially made for testing. The different file filters has special "evil" parts, like infinity loops that the system should handle.

---
+++ fileFilter/test/src/test-infinite-loop-and-data.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include <err.h>
+#include <stdlib.h>
+
+
+int main(void) {
+
+	int i = 0;
+
+	// Print some data
+	printf("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut\n");
+	printf("labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco\n");
+	printf("laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in\n");
+	printf("voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat\n");
+	printf("non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n");
+
+	// teh run forever.
+	while (1) {
+		i++;
+	}
+
+	return 0;
+}
---
+++ fileFilter/test/src/test-infinite-loop.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <err.h>
+#include <stdlib.h>
+
+
+int main(void) {
+
+	int i = 0;
+	while (1) {
+		i++;
+	}
+
+	return 0;
+}
